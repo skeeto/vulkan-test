1d215209ca4357f8fd1ea0a45a3010795fe989a1
Christopher Wellons <wellons@nullprogram.com>
2016-02-16 16:23:47 -0500

    Go even leaner!

--- vulkan_test.c
+++ vulkan_test.c
@@ -1,32 +1,31 @@
-#include <windows.h>
+__declspec(dllimport) void *LoadLibraryA(char *);
+__declspec(dllimport) void *GetProcAddress(void *, char *);
+__declspec(dllimport) int   MessageBoxA(void *, char *, char *, unsigned);
 
 #define REPORT(code, msg)                                         \
     do {                                                          \
-        MessageBoxA(0, msg, code ? "Failure" : "Success", MB_OK); \
-        ExitProcess(code);                                        \
+        MessageBoxA(0, msg, code ? "Failure" : "Success", 0);     \
+        return code;                                              \
     } while (0)
 
-int WINAPI
+int
 WinMainCRTStartup(void)
 {
-    HMODULE vk = LoadLibrary("vulkan-1.dll");
+    void *vk = LoadLibraryA("vulkan-1.dll");
     if (!vk)
         REPORT(-1, "Vulkan drivers not installed.");
     void *(*vkGetInstanceProcAddr)(void *, const char *) =
-        (void *)GetProcAddress(vk, "vkGetInstanceProcAddr");
+        GetProcAddress(vk, "vkGetInstanceProcAddr");
     if (!vkGetInstanceProcAddr)
-        REPORT(-1, "Vulkan drivers are malfunctioning "
-               "(vkGetInstanceProcAddr).");
+        REPORT(-1, "Vulkan is malfunctioning (vkGetInstanceProcAddr).");
     int (*vkCreateInstance)(int *, void *, void **) =
         vkGetInstanceProcAddr(0, "vkCreateInstance");
     if (!vkCreateInstance)
-        REPORT(-1, "Vulkan drivers are malfunctioning "
-               "(vkCreateInstance)");
+        REPORT(-1, "Vulkan is malfunctioning (vkCreateInstance)");
     void *instance = 0;
     int result = vkCreateInstance((int[16]){1}, 0, &instance);
     if (!instance || result != 0)
         REPORT(-1, "Vulkan drivers installed and functioning "
                "but are incompatible. An instance could not be created.");
     REPORT(0, "Successfully created a Vulkan instance.");
-    return 0;
 }
